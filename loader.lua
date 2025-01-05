1ddb574179ebb02ba794677566ac51c3731173be
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-05 13:11:45 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+local links = {
+    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua""
+    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
+    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua"
+}
+
+for _, link in ipairs(links) do
+    pcall(function()
+        loadstring(game:HttpGet(link))()
+    end)
+end
