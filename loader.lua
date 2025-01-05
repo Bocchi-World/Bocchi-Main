1dd13d68ecae7934b1294af33f6a27cdb3630fd4
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-05 13:14:00 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local links = {
-    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua""
+    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
     "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
     "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua"
 }
