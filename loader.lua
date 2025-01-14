3094864ee9c3a0f5c937edce51245a203ed5c0f2
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-14 12:40:38 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,17 @@
+
 local v3 = {
-    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
-    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
-    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua",
-    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua"
+    {url = "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua", delay = 0.5},
+    {url = "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua", delay = 0.5},
+    {url = "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua", delay = 0.5},
+    {url = "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua", delay = 0.5, requireFPS = true}
 }
 
 for v1, v2 in ipairs(v3) do
-    local success, result = pcall(function()
-        task.wait(0.5) 
-        return game:HttpGet(v2)
-    end)
-    
-    if success then
-        pcall(function()
-            loadstring(result)()
+    task.spawn(function()
+        if v2.requireFPS and not getgenv().FPS then return end
+        local success = pcall(function()
+            loadstring(game:HttpGet(v2.url))()
         end)
-    end
+        if not success then task.wait(v2.delay) end
+    end)
 end
