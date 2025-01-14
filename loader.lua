25a736014f57627fee31d73573458b443185ac6b
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-14 12:15:22 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,19 @@
 local v3 = {
-   "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
-   "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
-   "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua",
-   "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua"
+    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
+    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
+    "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua",
+    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua"
 }
 
 for v1, v2 in ipairs(v3) do
-   pcall(function() 
-       loadstring(game:HttpGet(v2))()
-   end)
+    local success, result = pcall(function()
+        task.wait(0.5) 
+        return game:HttpGet(v2)
+    end)
+    
+    if success then
+        pcall(function()
+            loadstring(result)()
+        end)
+    end
 end
