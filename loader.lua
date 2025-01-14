225741d3f338bfd63f47d1a005d89fabd93ca4af
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-14 09:56:44 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
-local links = {
+local v3 = {
     "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
     "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
     "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua"
     "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua"
 }
 
-for v1,v2 in ipairs(v2) do
+for v1,v2 in ipairs(v3) do
     pcall(function()
         loadstring(game:HttpGet(v2))()
     end)
