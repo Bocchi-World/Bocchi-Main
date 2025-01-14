8962b3b2f24d69dc0b281c67982fd922da2f6829
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-14 09:55:49 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local links = {
     "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/UI.lua",
     "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/pickcard.lua",
     "https://raw.githubusercontent.com/Ngducok/doing-some-shit/refs/heads/main/hey.lua"
+    "https://raw.githubusercontent.com/Bocchi-World/Bocchi-Main/refs/heads/main/FPSBooster.lua"
 }
 
 for _, link in ipairs(links) do
