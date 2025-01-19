b5a05b7f4affe42a330cdf18114eed778bf18cd3
Jung Ganmyeon <148054296+junggamyeon@users.noreply.github.com>
2025-01-19 23:42:01 +0700

    Create unit.lua

---
+++ unit.lua
@@ -0,0 +1,13 @@
+local UnitsRequirement = {
+    ghostkun = {
+        TakeDown = 7500,
+        Yellow_Fruit = 30,
+        Blue_Fruit = 13,
+        Red_Fruit = 13,
+        Pink_Fruit = 10,
+        Green_Fruit = 10,
+        Rainbow_Fruit = 3,
+    }
+}
+
+return UnitsRequirement
